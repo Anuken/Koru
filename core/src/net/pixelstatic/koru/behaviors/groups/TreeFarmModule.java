072d67a4ce79c501c6a1ba6a5c193bc754ff495e
BuildTools <arnukren@gmail.com>
2016-05-02 19:47:17 -0400

    Pushing normal branch.

--- core/src/net/pixelstatic/koru/behaviors/groups/TreeFarmModule.java
+++ core/src/net/pixelstatic/koru/behaviors/groups/TreeFarmModule.java
@@ -1,48 +1,57 @@
 package net.pixelstatic.koru.behaviors.groups;
 
-import net.pixelstatic.koru.Koru;
 import net.pixelstatic.koru.behaviors.groups.Group.PointType;
 import net.pixelstatic.koru.behaviors.groups.Group.TilePoint;
 import net.pixelstatic.koru.behaviors.tasks.BreakBlockTask;
 import net.pixelstatic.koru.behaviors.tasks.GrowPlantTask;
 import net.pixelstatic.koru.behaviors.tasks.PlaceBlockTask;
+import net.pixelstatic.koru.modules.World;
 import net.pixelstatic.koru.utils.Point;
 import net.pixelstatic.koru.world.Material;
 import net.pixelstatic.koru.world.PinetreeTileData;
 
 import com.badlogic.gdx.utils.Array;
 
-
 public class TreeFarmModule extends GroupModule{
 	int trees = 0;
-	
+
 	@Override
 	public void update(){
-		
-		if(trees < 6){
+
+		if(trees < 5){
 			Point water = world.search(Material.water, group.x, group.y, 50); //search for water
 			if(water == null) return;
 			int wx = water.x, wy = water.y;
-			
-			int rx = -1, ry = 0;
-			Koru.log(water);
-			for(int i = 1; i <= 6; i ++){
-				Koru.log((wx + rx*i) + "," +  (wy + ry*i) + "(" + rx + "," + ry + ")");
-				group.addPoint(PointType.treefarm, wx + rx*i, wy + ry*i);
-				trees ++;
+
+			int size = 3;
+
+			for(int x = -size; x <= size; x ++){
+				for(int y = -size; y <= size; y ++){
+					int worldx = water.x + x;
+					int worldy = water.y + y;
+					if( !World.inBounds(worldx, worldy)) continue;
+					if( !world.blockSolid(worldx, worldy)){
+						if(worldy % 2 == 0){
+							group.addPoint(PointType.treefarm, worldx, worldy);
+							trees ++;
+						}else{
+							new PlaceBlockTask(worldx, worldy, Material.woodfloor).add(tasks);
+						}
+					}
+				}
 			}
-		}else{
+		}else if(tasks.size == 0){
 			Array<TilePoint> points = group.points(PointType.treefarm);
 			for(TilePoint point : points){
-				if(point.reserved()) continue;
+				//if(point.reserved()) continue;
 				Material material = point.block();
-
-				if(material == Material.pinesapling && !point.tile().getBlockData(PinetreeTileData.class).hasEnoughWater()){
+				if(point.tile().tile != Material.grass){
+					new PlaceBlockTask(point.x, point.y, Material.grass).add(tasks);
+				}else if(material == Material.pinesapling && !point.tile().getBlockData(PinetreeTileData.class).hasEnoughWater()){
 					new GrowPlantTask(point.x, point.y).add(tasks);
 				}else if(material.name().contains("pinetree")){
 					new BreakBlockTask(material, point.x, point.y).add(tasks);
 				}else if(material != Material.pinesapling){
-					Koru.log("placing block at " + point.x + " " + point.y);
 					new PlaceBlockTask(point.x, point.y, Material.pinesapling).add(tasks);
 				}
 			}
